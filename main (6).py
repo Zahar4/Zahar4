40472e00f3a00888d34ecf0cef1a80989b44950e
Zahar4 <159257511+Zahar4@users.noreply.github.com>
2024-02-28 14:35:47 +0300

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,20 @@
+#21
+# 1)
+from math import sqrt,sin,exp,pi 
+x=int(input("Введите число x:"))
+if x < 0:
+    print("(1)",4)
+if 0 <=x<1:
+    print(X**2+3*x+4)
+if x > 1:
+    print(x+7)
+# 2)
+a=int(input("Введите число а: "))
+b=int(input("Введите число b: "))
+x=int(input("Введите число X: "))
+if x < 0:
+    print(math.sin(x+(math.pi/2))**2)
+if a<=x<=b:
+    print (((math.exp)**2)*math.sin(x+(math.pi/2)))
+if x > b:
+    print(math.sqrt(math.sin(x+math.pi/3)))
